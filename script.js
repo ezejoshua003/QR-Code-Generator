0751d9a6b38988760162c80155c610ee2089d609
ezejoshua003 <dyoungchai@gmail.com>
2022-07-18 03:20:27 +0900

    commit

--- script.js
+++ script.js
@@ -2,22 +2,23 @@ const wrapper = document.querySelector(".wrapper"),
 qrInput = wrapper.querySelector(".form input"),
 generateBtn = wrapper.querySelector(".form button"),
 qrImg = wrapper.querySelector(".qr-code img");
+let preValue;
 
 generateBtn.addEventListener("click", () => {
-    let qrValue = qrInput.value;
-    if(!qrValue) return; // if the input is empty then return from here
-    generateBtn.innerText = "Generating QR code...";
-    //getting a QR code of user entered value using the qrserver
-    //api and passing the api returned img src to qrImg
-    qrImg.src = 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrValue}';
-    qrImg.addEventListener("load",() =>{ // once QR code img loaded
+    let qrValue = qrInput.value.trim();
+    if(!qrValue || preValue === qrValue) return;
+    preValue = qrValue;
+    generateBtn.innerText = "Generating QR Code...";
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${qrValue}`;
+    qrImg.addEventListener("load", () => {
         wrapper.classList.add("active");
         generateBtn.innerText = "Generate QR Code";
     });
 });
 
-qrInput.addEventListener("keyup",() => {
-    if(!qrInput.value) {
+qrInput.addEventListener("keyup", () => {
+    if(!qrInput.value.trim()) {
         wrapper.classList.remove("active");
+        preValue = "";
     }
 });
